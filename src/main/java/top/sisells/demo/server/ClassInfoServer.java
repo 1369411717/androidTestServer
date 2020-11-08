dc04fff0a249f8fe9b069196804846705d546e20
大江户时代 <1369411717@qq.com>
2020-11-09 02:50:38 +0800

    6

--- src/main/java/top/sisells/demo/server/ClassInfoServer.java
+++ src/main/java/top/sisells/demo/server/ClassInfoServer.java
@@ -1,6 +1,9 @@
 package top.sisells.demo.server;
 
 
+import top.sisells.demo.pojo.ClassInfo;
+import java.util.List;
+
 public interface ClassInfoServer {
-    
+    List<ClassInfo> selectAllClassInfo(String semester, String leftStudentModel);
 }
--- src/main/java/top/sisells/demo/server/impl/ClassInfoServerImpl.java
+++ src/main/java/top/sisells/demo/server/impl/ClassInfoServerImpl.java
@@ -1,6 +1,19 @@
 package top.sisells.demo.server.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import top.sisells.demo.dao.ClassInfoSql;
+import top.sisells.demo.pojo.ClassInfo;
 import top.sisells.demo.server.ClassInfoServer;
 
+import java.util.List;
+
+@Service
 public class ClassInfoServerImpl implements ClassInfoServer {
+    @Autowired
+    private ClassInfoSql classInfoSql;
+
+    public List<ClassInfo> selectAllClassInfo(String semester, String leftStudentModel){
+        return classInfoSql.selectAllClassInfo(semester,leftStudentModel);
+    }
 }
